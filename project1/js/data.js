06006a1d04e045364944ad9ca9ef02aa6c906faf
hossamelhadad <42821031+hossamelhadad@users.noreply.github.com>
2021-12-31 17:58:44 +0200

    Update data.js

--- project1/js/data.js
+++ project1/js/data.js
@@ -3,10 +3,13 @@ var electronics;
 var jewelery;
 var men;
 var woman;
+var page;
 
 
 // function to get all prouducts
 function getdata() {
+
+
     fetch("https://fakestoreapi.com/products")
       .then(res => res.json())
       .then(data => obj = data)
@@ -23,19 +26,40 @@ function splitCategory() {
   console.log(men);
   woman=obj.filter(data=> data.category == "women's clothing");
   console.log(woman);
-  createCards();
+// to cheack which page is called
+  switch (page) {
+    case 'Accessories':
+    createCards(jewelery);
+      break;
+      case 'Men':
+    createCards(men);
+      break;
+      case 'Women':
+    createCards(woman);
+      break;
+      case 'Electronics':
+    createCards(electronics);
+      break;
+  
+    default:
+      break;
+  }
+  
 }
 
-
-function createCards() {
+// function to cread card in html pages
+function createCards(categoryType) {
+  console.log(categoryType);
   
-  for (let index = 0; index < men.length; index++) {
+  for (let index = 0; index < categoryType.length; index++) {
    
     
 // create card element
 //card
 let cardDiv = document.createElement("div");
 cardDiv.className='col-lg-4 col-md-6';
+cardDiv.id=index;
+
 //cardbody
 let cardBody=document.createElement('div');
 cardBody.className='product__item';
@@ -44,7 +68,7 @@ imgDiv=document.createElement('div');
 imgDiv.className='product__item__pic set-bg';
 // imge 
 imgElement=document.createElement('img');
-imgElement.src=men[index].image;
+imgElement.src=categoryType[index].image;
 //
 
  // cart
@@ -53,7 +77,8 @@ imgElement.src=men[index].image;
  // first li
  liElement=document.createElement('li');
  aElement=document.createElement('a');
- aElement.href=men[index].image;
+ aElement.href=categoryType[index].image;
+
  aElement.className='image-popup';
  imgCard=document.createElement('span');
  imgCard.className='fas fa-expand-alt';
@@ -70,6 +95,7 @@ liElement=document.createElement('li');
  //aElement.href=men[index].image;
  imgCard=document.createElement('span');
  imgCard.className='far fa-heart';
+ aElement.appendChild(imgCard);
 
  //add a to li
 liElement.appendChild(aElement);
@@ -79,9 +105,10 @@ ulElement.appendChild(liElement);
  // last li
 liElement=document.createElement('li');
 aElement=document.createElement('a');
-//aElement.href=men[index].image;
+//aElement.href=categoryType[index].image;
 imgCard=document.createElement('span');
 imgCard.className='fas fa-shopping-cart';
+aElement.appendChild(imgCard);
 
 //add a to li
 liElement.appendChild(aElement);
@@ -103,18 +130,18 @@ atext=document.createElement('a');
 // stars
 stardiv=document.createElement('div');
 stardiv.className= 'rating';
-starIcon=createElement('i');
+starIcon=document.createElement('i');
 starIcon.className='fa fa-star';
-stardiv.appendChild(starIcon);
-starIcon.className='fa fa-star';
-stardiv.appendChild(starIcon);
-starIcon.className='fa fa-star';
-stardiv.appendChild(starIcon);
-starIcon.className='fa fa-star';
-stardiv.appendChild(starIcon);
+// to add stars
+for (let x = 0; x < categoryType[index].rating.rate; x++) {
+var stars =starIcon.cloneNode(true);
+stardiv.appendChild(stars);
+}
+
+
 
 //atext.href='#';
-let productTitle=document.createTextNode(men[index].title);
+let productTitle=document.createTextNode(categoryType[index].title);
 atext.appendChild(productTitle);
 //add title to content div
 contentDiv.appendChild(atext);
@@ -123,7 +150,7 @@ contentDiv.appendChild(stardiv);
 //price
 priceElement=document.createElement('div');
 priceElement.className='product__price';
-let productPrice=document.createTextNode(men[index].price+"$");
+let productPrice=document.createTextNode(categoryType[index].price+"$");
 priceElement.appendChild(productPrice);
 //add price element to content div
 contentDiv.appendChild(priceElement);
@@ -137,9 +164,51 @@ cardBody.appendChild(contentDiv);
 // add card body to card dive
 cardDiv.appendChild(cardBody);
 
-var cardRow = document.getElementById('products');
+var cardRow = document.getElementById('Products');
+
 cardRow.appendChild(cardDiv);
+
+cardDiv.onclick= function (){
+  displayProduct(cardDiv.id,categoryType);
+  
+}
   }
 
 
 }
+
+
+
+
+
+
+
+
+
+// display product in product page
+function displayProduct(index,categ) {
+  
+  localStorage.setItem('id',categ[index].id);
+  localStorage.setItem('title',categ[index].title);
+  localStorage.setItem('price',categ[index].price);
+  localStorage.setItem('description',categ[index].description);
+  localStorage.setItem('category',categ[index].category);
+  localStorage.setItem('image',categ[index].image);
+  localStorage.setItem('rate',categ[index].rating.rate);
+  localStorage.setItem('count',categ[index].rating.count);
+  window.open('product.html');
+
+
+
+}
+
+
+ function setcate(categoo) {
+   getdata();
+
+   page=categoo;
+   
+   
+ } 
+
+
