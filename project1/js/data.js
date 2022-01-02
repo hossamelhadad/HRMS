660f78f21a62caff026d4f28e4df71a498abc9ce
hossamelhadad <42821031+hossamelhadad@users.noreply.github.com>
2022-01-02 11:25:22 +0200

    Update data.js

--- project1/js/data.js
+++ project1/js/data.js
@@ -93,13 +93,21 @@ function displayProduct(index,categ) {
 
 
  function cheackOut(current , prou) {
-  var titles ='title'+current;
-  var prices= 'price'+current;
-  var imges= 'image'+current;
-  console.log(titles);
-  localStorage.setItem(titles,prou[current].title);
-  localStorage.setItem(prices,prou[current].price);
-  localStorage.setItem(imges,prou[current].image);
+   
+     if (localStorage.loginValid) {
+        var titles ='title'+current;
+        var prices= 'price'+current;
+        var imges= 'image'+current;
+        console.log(titles);
+        localStorage.setItem(titles,prou[current].title);
+        localStorage.setItem(prices,prou[current].price);
+        localStorage.setItem(imges,prou[current].image);
+     }
+     else{
+        window.location.href = "login.html";   
+
+     }
+  
 
 
  }
@@ -300,3 +308,96 @@ function displayProduct(index,categ) {
      return Math.ceil(currentPage.length / records_per_page);
  }
  
+
+
+
+
+
+
+
+
+
+
+
+ var loginValid = true;
+
+//checkName
+function checkName(e) {
+    var regex = new RegExp("^[0-9]+$");
+    var key = String.fromCharCode(e.which);
+    console.log(key);
+    if (regex.test(key)) {
+       e.preventDefault();
+       return false;
+    }
+    //return (key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z');
+}
+
+var validname = false, validpass = false, username, pass;
+
+function signUp(e){
+
+    if(validname == true && validpass == true)
+    {
+        localStorage.setItem('username',username);
+        localStorage.setItem('password',pass);
+
+        window.location.href = "logins.html";   
+    }
+    else {
+        e.preventDefault();
+        alert("Plz correct the validation errors first");
+        return ;
+    }
+}
+function blr(x){
+    if(x.value.length > 3){   
+        validname = true;
+        username = x.value;
+        document.getElementById('invalidname').innerHTML="<i class='fas fa-check'></i>";
+    }
+    else{
+        document.getElementById('invalidname').innerHTML="<i class='glyphicon glyphicon-remove'></i>";
+    }
+}
+function checkp(x){
+    if(x.value.length < 4){
+        document.getElementById('invalidpass').innerHTML = "<i class='glyphicon glyphicon-remove'></i>";
+    }
+    else{
+        document.getElementById('invalidpass').innerHTML = "<i class='fas fa-check'></i>";
+    }
+}
+function checkiden(x){
+    if(document.getElementById('pass').value != null && (document.getElementById('pass').value === document.getElementById('rpass').value)){
+        validpass = true;
+        pass = x.value;
+        document.getElementById('invalidRpass').innerHTML = "<i class='fas fa-check'></i></br>";
+        document.getElementById('invalidtext').innerHTML = "PerfectO!";
+    }
+    else{
+       document.getElementById('invalidRpass').innerHTML = "<i class='glyphicon glyphicon-remove'></i></br>";
+       document.getElementById('invalidtext').innerHTML = "NotSame!";
+    }
+}
+
+function logins(){
+    console.log("login")
+    if((document.getElementById('loginName').value == localStorage.username) && (document.getElementById('loginPass').value==localStorage.password)){
+        alert(' vaild');
+
+        localStorage.setItem('loginValid',loginValid);
+        window.close();
+
+        window.open('home.html');
+    }
+    else
+    {
+        alert(' imvaild');
+
+        console.log('in vaild')
+        localStorage.setItem('loginValid','false')
+        
+ 
+    }
+}
