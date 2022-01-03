8c73084f6379b2d370108e715e26c2412c0d623c
hossamelhadad <42821031+hossamelhadad@users.noreply.github.com>
2022-01-03 23:39:27 +0200

    Update project.js

--- project1/js/project.js
+++ project1/js/project.js
@@ -1,74 +1,10 @@
-function getproduct(){
-    window.open('product.html',"_self");
-}
+$(window).on("load", function () {
+    $(".loader-wrapper").fadeOut("slow");
+});
 
 
-//checkName
-function checkName(e) {
-    var regex = new RegExp("^[0-9]+$");
-    var key = String.fromCharCode(e.which);
-    console.log(key);
-    if (regex.test(key)) {
-       e.preventDefault();
-       return false;
-    }
-    //return (key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z');
-}
-
-var validname = false, validpass = false, username, pass;
-
-function signUp(e){
-
-    if(validname == true && validpass == true)
-    {
-        window.location.href = "home.html";   
-    }
-    else {
-        e.preventDefault();
-        alert("Plz correct the validation errors first");
-        return ;
-    }
-}
-function blr(x){
-    if(x.value.length > 3){   
-        validname = true;
-        username = x.value;
-        document.getElementById('invalidname').innerHTML="<i class='fas fa-check'></i>";
-    }
-    else{
-        document.getElementById('invalidname').innerHTML="<i class='glyphicon glyphicon-remove'></i>";
-    }
-}
-function checkp(x){
-    if(x.value.length < 4){
-        document.getElementById('invalidpass').innerHTML = "<i class='glyphicon glyphicon-remove'></i>";
-    }
-    else{
-        document.getElementById('invalidpass').innerHTML = "<i class='fas fa-check'></i>";
-    }
-}
-function checkiden(x){
-    if(document.getElementById('pass').value != null && (document.getElementById('pass').value === document.getElementById('rpass').value)){
-        validpass = true;
-        pass = x.value;
-        document.getElementById('invalidRpass').innerHTML = "<i class='fas fa-check'></i></br>";
-        document.getElementById('invalidtext').innerHTML = "PerfectO!";
-    }
-    else{
-       document.getElementById('invalidRpass').innerHTML = "<i class='glyphicon glyphicon-remove'></i></br>";
-       document.getElementById('invalidtext').innerHTML = "NotSame!";
-    }
-}
-function login(){
-    if((document.getElementById('fullname').value == username) && (document.getElementById('password').value==pass)){
-        window.location.href = "home.html"; 
-    }
-}
-
-
-
-// discount function
-var finalDate = new Date("Jan 5, 2022 15:37:25").getTime();
+function discountsale() {
+    var finalDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
 var x = setInterval(function () {
 
@@ -94,6 +30,12 @@ var x = setInterval(function () {
         document.getElementById('expired').innerHTML = "EXPIRED";
     }
 }, 1000);
+
+    
+}
+
+
+// discount function
 
 
 // contact function
